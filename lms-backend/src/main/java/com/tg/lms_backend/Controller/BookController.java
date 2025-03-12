041b95a1f91adede17eb954e315af7d03de72b1d
TG Ler <lertg1@gmail.com>
2025-03-13 00:33:09 +0800

    2

--- lms-backend/src/main/java/com/tg/lms_backend/Controller/BookController.java
+++ lms-backend/src/main/java/com/tg/lms_backend/Controller/BookController.java
@@ -1,5 +1,42 @@
 package com.tg.lms_backend.Controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tg.lms_backend.Model.Book;
+
+@RestController
+@RequestMapping("/api/books")
 public class BookController {
+
+	@Autowired
+	private BookService bookService;
+	
+	@GetMapping
+	public List<Book> getAllBooks(){
+		return bookService.getAllBooks();
+	}
+	
+	@GetMapping("/{id}")
+	public Book getBookById(@PathVariable int id) {
+		return bookService.getBookById(id);
+	}
+	
+	@PostMapping
+	public Book createBook(@RequestBody Book book ) {
+		return bookService.saveBook(book);
+	}
+	
+	@PutMapping
+	public Book updateBook(@PathVariable int id, )
+	
 
 }

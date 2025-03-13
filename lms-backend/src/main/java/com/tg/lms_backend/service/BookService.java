69a46f01ea8b746fc60e807423d43caa3745e55c
TG Ler <lertg1@gmail.com>
2025-03-14 00:09:01 +0800

    14032025

--- lms-backend/src/main/java/com/tg/lms_backend/service/BookService.java
+++ lms-backend/src/main/java/com/tg/lms_backend/service/BookService.java
@@ -28,20 +28,37 @@ public class BookService {
 	
 	public Book updateBook(int id, Book bookDetails) {
 		Book book = bookRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Book not found with id "+id));
-		if (book !=null) {
+		if(bookDetails.getBookTitle() !=null) {
 			book.setBookTitle(bookDetails.getBookTitle());
-			book.setAuthor(bookDetails.getAuthor());
-			book.setIsbn(bookDetails.getIsbn());
-			book.setCategory(bookDetails.getCategory());
-			book.setBookCoverUrl(bookDetails.getBookCoverUrl());
-			book.setCallNumber(bookDetails.getCallNumber());
-			book.setDueDate(bookDetails.getDueDate());
-			book.setGenre(bookDetails.getGenre());
-			book.setLoaned(bookDetails.getLoaned());
-			book.setPublisher(bookDetails.getPublisher());
-			return bookRepository.save(book);
 		}
-		return null;
+		if(bookDetails.getAuthor() !=null) {
+			book.setAuthor(bookDetails.getAuthor());
+		}
+		if(bookDetails.getIsbn() !=null) {
+			book.setIsbn(bookDetails.getIsbn());
+		}
+		if(bookDetails.getCategory() !=null) {
+			book.setCategory(bookDetails.getCategory());
+		}
+		if(bookDetails.getBookCoverUrl() !=null) {
+			book.setBookCoverUrl(bookDetails.getBookCoverUrl());
+		}
+		if(bookDetails.getCallNumber() !=null) {
+			book.setCallNumber(bookDetails.getCallNumber());
+		}
+		if(bookDetails.getDueDate() !=null) {
+			book.setDueDate(bookDetails.getDueDate());
+		}
+		if(bookDetails.getGenre() !=null) {
+			book.setGenre(bookDetails.getGenre());
+		}
+		if(bookDetails.getLoaned() !=null) {
+			book.setLoaned(bookDetails.getLoaned());
+		}
+		if(bookDetails.getPublisher() !=null) {
+			book.setPublisher(bookDetails.getPublisher());
+		}
+			return bookRepository.save(book);
 	}
 	
 	public void deleteBook(int id) {

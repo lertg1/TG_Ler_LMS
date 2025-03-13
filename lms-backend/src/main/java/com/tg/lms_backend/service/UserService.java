834a92e0277f2de04b9e8ade553997e1293fba14
TG Ler <lertg1@gmail.com>
2025-03-13 23:39:51 +0800

    48

--- lms-backend/src/main/java/com/tg/lms_backend/service/UserService.java
+++ lms-backend/src/main/java/com/tg/lms_backend/service/UserService.java
@@ -16,26 +16,37 @@ public class UserService {
 	
 	public User saveUser(User user) {
 		// TODO Auto-generated method stub
-		return null;
+		return userRepository.save(user);
 	}
 
 	public List<User> getAllUsers() {
 		// TODO Auto-generated method stub
-		return null;
+		return userRepository.findAll();
 	}
 
 	public User getUserById(int id) {
 		// TODO Auto-generated method stub
-		return null;
+		return userRepository.findById(id).orElse(null);
 	}
 
 	public User updateUser(int id, User userDetails) {
 		// TODO Auto-generated method stub
+		User user = userRepository.findById(id).orElse(null);
+		if(user!=null) {
+			user.setUserName(userDetails.getUserName());
+			user.setUserPassword(userDetails.getUserPassword());
+			user.setUserEmail(userDetails.getUserEmail());
+			user.setUserStatus(userDetails.getUserStatus());
+			user.setUpdatedAt(userDetails.getUpdatedAt());		
+			return userRepository.save(user);
+		}
 		return null;
+		
 	}
 
 	public void deleteUser(int id) {
 		// TODO Auto-generated method stub
+		userRepository.deleteById(id);;
 		
 	}
 

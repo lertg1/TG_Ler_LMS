f2a236e1abdc8416caa564a845c1314a9f50d502
TG Ler <lertg1@gmail.com>
2025-03-15 02:51:12 +0800

    gg

--- lms-backend/src/main/java/com/tg/lms_backend/config/SecurityConfig.java
+++ lms-backend/src/main/java/com/tg/lms_backend/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
         http
             .csrf(AbstractHttpConfigurer::disable)
             .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/api/**").permitAll()
                 .requestMatchers("/api/auth/**").permitAll()
                 .anyRequest().authenticated()
             )
--- lms-backend/src/main/java/com/tg/lms_backend/controller/AuthController.java
+++ lms-backend/src/main/java/com/tg/lms_backend/controller/AuthController.java
@@ -1,14 +1,12 @@
 package com.tg.lms_backend.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tg.lms_backend.model.LoginRequest;
 import com.tg.lms_backend.model.User;
 import com.tg.lms_backend.service.UserService;
 
@@ -29,8 +27,8 @@ public class AuthController {
 	}
 	
 	@PostMapping("/login")
-	public User authenticateUser(@RequestBody LoginRequest loginRequest) {
-		return userService.authenticateUser(loginRequest.getEmail(), loginRequest.getPassword(), passwordEncoder);
+	public User authenticateUser(@RequestBody User user) {
+		return userService.authenticateUser(user.getUserEmail(), user.getUserPassword(), passwordEncoder);
 
 	}
 
